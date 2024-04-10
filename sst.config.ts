2c0801271de3b87adf98c04891d7e52903cfd2aa
Ryan Lee <drdgvhbh@gmail.com>
2024-04-10 03:36:36 -0400

    chore: fix permissions

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,6 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+import pulumi from "@pulumi/pulumi";
 import { RemotionLambda } from "./src";
 
 export default $config({
@@ -28,9 +29,81 @@ export default $config({
         REMOTION_FUNCTION_NAME: remotion.function.name,
         REMOTION_SITE_URL: remotion.siteUrl,
         REMOTION_BUCKET_NAME: remotion.bucket.bucket,
-        // Todo: don't use admin aws keys
-        REMOTION_AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID!,
-        REMOTION_AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY!,
+      },
+      transform: {
+        server: (server) => {
+          /**
+         * Combine linked permissions with remotion execution role policy
+         * 
+         * @see https://www.remotion.dev/docs/lambda/without-iam/#1-create-role-policy
+         */
+          server.permissions = pulumi.output(server.permissions).apply((
+            permissions,
+          ) => [...(permissions ?? []), {
+            actions: [
+              "servicequotas:GetServiceQuota",
+              "servicequotas:GetAWSDefaultServiceQuota",
+              "servicequotas:RequestServiceQuotaIncrease",
+              "servicequotas:ListRequestedServiceQuotaChangeHistoryByQuota",
+            ],
+            resources: ["*"],
+          }, {
+            actions: [
+              "iam:SimulatePrincipalPolicy",
+            ],
+            resources: ["*"],
+          }, {
+            actions: ["iam:PassRole"],
+            resources: ["arn:aws:iam::*:role/remotion-lambda-role"],
+          }, {
+            actions: [
+              "s3:GetObject",
+              "s3:DeleteObject",
+              "s3:PutObjectAcl",
+              "s3:PutObject",
+              "s3:CreateBucket",
+              "s3:ListBucket",
+              "s3:GetBucketLocation",
+              "s3:PutBucketAcl",
+              "s3:DeleteBucket",
+              "s3:PutBucketOwnershipControls",
+              "s3:PutBucketPublicAccessBlock",
+              "s3:PutLifecycleConfiguration",
+            ],
+            resources: ["arn:aws:s3:::remotionlambda-*"],
+          }, {
+            actions: ["s3:ListAllMyBuckets"],
+            resources: ["*"],
+          }, {
+            actions: ["lambda:ListFunctions", "lambda:GetFunction"],
+            resources: ["*"],
+          }, {
+            actions: [
+              "lambda:InvokeAsync",
+              "lambda:InvokeFunction",
+              "lambda:CreateFunction",
+              "lambda:DeleteFunction",
+              "lambda:PutFunctionEventInvokeConfig",
+              "lambda:PutRuntimeManagementConfig",
+              "lambda:TagResource",
+            ],
+            resources: ["arn:aws:lambda:*:*:function:remotion-render-*"],
+          }, {
+            actions: [
+              "logs:CreateLogGroup",
+              "logs:PutRetentionPolicy",
+            ],
+            resources: [
+              "arn:aws:logs:*:*:log-group:/aws/lambda/remotion-render-*",
+            ],
+          }, {
+            actions: ["lambda:GetLayerVersion"],
+            resources: [
+              "arn:aws:lambda:*:678892195805:layer:remotion-binaries-*",
+              "arn:aws:lambda:*:580247275435:layer:LambdaInsightsExtension*",
+            ],
+          }]);
+        },
       },
     });
   },
