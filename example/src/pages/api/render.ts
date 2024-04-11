e4853e4f42a8ea5eab04a6a907d192d36e99781a
Karel <nagelkarel@gmail.com>
2024-04-11 16:38:30 +0300

    2 apis

--- example/src/pages/api/render.ts
+++ example/src/pages/api/render.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro";
-import { renderMediaOnLambda, getRenderProgress } from "@remotion/lambda/client";
+import { renderMediaOnLambda } from "@remotion/lambda/client";
 import { Resource } from "sst";
 import { DURATION_IN_FRAMES } from "../../../remotion/Root";
 
@@ -17,24 +17,7 @@ export const POST: APIRoute = async () => {
     // To avoid hitting AWS concurrency limit on new accounts https://www.remotion.dev/docs/lambda/troubleshooting/rate-limit#exception-new-accounts-using-aws-lambda
     framesPerLambda: DURATION_IN_FRAMES / MAX_LAMBDAS,
   });
-
-  while (true) {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    const progress = await getRenderProgress({
-      renderId: res.renderId,
-      bucketName: Resource.Remotion.bucketName,
-      functionName: Resource.Remotion.functionName,
-      region: Resource.Remotion.region as any,
-    });
-
-    if (progress.done)
-      return new Response(JSON.stringify(progress), {
-        headers: { "Content-Type": "application/json" },
-      });
-    else if (progress.fatalErrorEncountered)
-      return new Response(JSON.stringify(progress), {
-        headers: { "Content-Type": "application/json" },
-        status: 500,
-      });
-  }
+  return new Response(JSON.stringify({ renderId: res.renderId }), {
+    headers: { "Content-Type": "application/json" },
+  });
 };
