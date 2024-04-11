9a9f6c14d69c05b0fd5b26abf2c0e5d0eda47c38
Karel <nagelkarel@gmail.com>
2024-04-11 18:50:32 +0300

    shiki highlighter

--- example/remotion/Composition.tsx
+++ example/remotion/Composition.tsx
@@ -1,5 +1,17 @@
 import { AbsoluteFill, interpolate, useCurrentFrame, useVideoConfig } from "remotion";
 import { z } from "zod";
+import { useEffect, useState } from "react";
+import { HEIGHT, WIDTH } from "./Root";
+import { getHighlighterCore, type HighlighterCore } from "shiki/core";
+import loadWasm from "shiki/wasm";
+import nord from "shiki/themes/nord.mjs";
+import ts from "shiki/langs/typescript.mjs";
+
+const highlighterPromise = getHighlighterCore({
+  themes: [nord],
+  langs: [ts],
+  loadWasm,
+});
 
 export const myCompSchema = z.object({
   framework: z.string(),
@@ -27,9 +39,24 @@ export const MyComposition: React.FC<z.infer<typeof myCompSchema>> = ({
   const { durationInFrames } = useVideoConfig();
   const text = code(framework, remotionPath);
   const char = interpolate(frame, [0, durationInFrames - 40], [0, text.length]);
+  const [highlighter, setHighlighter] = useState<HighlighterCore>();
+  useEffect(() => {
+    highlighterPromise.then(setHighlighter);
+  }, []);
   return (
     <AbsoluteFill className="items-center justify-center text-white">
-      <code>{text.slice(0, char)}</code>
+      {highlighter && (
+        <div
+          style={{ width: WIDTH, height: HEIGHT }}
+          className="h-full w-full text-4xl"
+          dangerouslySetInnerHTML={{
+            __html: highlighter.codeToHtml(
+              text.slice(0, char) + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
+              { lang: "ts", theme: "nord" },
+            ),
+          }}
+        />
+      )}
     </AbsoluteFill>
   );
 };
